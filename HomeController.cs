313214d37c9410a29bff3ae341a0ae64f4e0603e
crazysunkown <135548722+crazysunkown@users.noreply.github.com>
2023-06-07 23:51:31 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using prac1.Models;
+
+namespace prac1.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> index()
+    {
+        var model=new StockQuote {System="Hello", Price=6000};
+        return View(model);
+    }
+}
---
+++ StockQuote.cs
@@ -0,0 +1,10 @@
+using String;
+namespace prac1.Models
+{
+    public class StockQuote{
+        public string System
+        {get; set;}
+        public int Price
+        {get; set;}
+    }
+}
